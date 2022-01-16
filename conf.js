4cb2e73fc74d75818b6396714cd5aa8dbe190c2b
jsptt <31617952+jspgg@users.noreply.github.com>
2022-01-16 18:53:10 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -54,8 +54,8 @@ jsproxy_config({
   /**
    * 默认节点
    */
-  // node_default: 'mysite',
-  node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
+   node_default: 'mysite',
+  //node_default: /github\.io$/.test(location.host) ? 'demo-hk' : 'mysite',
 
   /**
    * 加速节点
@@ -66,10 +66,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  //assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  // assets_cdn: 'assets/',
+   assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
